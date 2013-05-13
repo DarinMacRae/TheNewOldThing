3b746dcc31f974ad5458c53d4aca1202cf5c72e8
Darin Creason <darin.creason@gmail.com>
2013-05-13 09:41:02 -0700

    Outline

--- TheNewOldThing/TheNewOldThing/Program.cs
+++ TheNewOldThing/TheNewOldThing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace TheNewOldThing
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadAllLines(args[0]);
+            var summary = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                var splits = line.Split(',');
+
+                if (!summary.ContainsKey(splits[1]))
+                    summary.Add(splits[1], 0);
+
+                summary[splits[1]]++;
+            }
+
+            foreach (var item in summary)
+            {
+                Console.WriteLine("{0},{1}", item.Key, item.Value);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
